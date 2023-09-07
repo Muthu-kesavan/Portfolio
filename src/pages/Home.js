b63cf748485e1d36bd9952fb8efe76127a126e0d
Muthu kesavan S <muthukesavan6044@gmail.com>
2023-09-07 15:39:48 +0530

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ function Home(){
     return (
         <div className="home">
             <div className="about">
-                <h2>Hello,<img src={hand} width="85px"></img> I'm Muthu Kesavan</h2>
+                <h2>Hi,<img src={hand} width="68px"></img> I am Muthu Kesavan</h2>
                 <div className="prompt">
                     <p>A Final year computer science student with strong passion of learning and building new things. </p>
                 <a href="https://www.linkedin.com/in/muthu-kesavan-s-95425821b/" target="_blank" rel="noopener noreferrer">
@@ -32,25 +32,25 @@ function Home(){
                     <li className="item">
                         <h2>Front-End</h2>
                         <span>
-                            HTML, CSS, React, MaterialUI.   </span>
+                            HTML, CSS, React, MaterialUI   </span>
                     </li>
                     <li className="item">
                         <h2>Back-End</h2>
                         <span>
-                            MongoDB.
+                            MongoDB
                         </span>
                     </li>
                     <li className="item">
                         <h2>Programming languages</h2>
-                        <span> Python, Javascript.</span>
+                        <span> Python, Javascript</span>
                     </li>
                     <li className="item">
                         <h2>Cyber-security Tools</h2>
-                        <span>Nmap, Wireshark, Metasploit, lazyscript, Autospy.</span>
+                        <span>Nmap, Wireshark, Metasploit, lazyscript</span>
                     </li>
                     <li className="item">
                         <h2>Operating systems</h2>
-                        <span>Windows, Kali Linux.</span>
+                        <span>Windows, Kali Linux</span>
                     </li>
                     </ol>
             </div>
